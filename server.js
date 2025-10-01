c76b5d00101c143a196f8de40f86b71bc28b0b23
Sid1005 <siddharth.ceri@gmail.com>
2025-10-01 21:44:18 +0530

    Fixed .env again lol

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ import express from 'express';
 import cors from 'cors';
 import twilio from 'twilio';
 
-// Load environment variables
-dotenv.config();
-
 const app = express();
 app.use(cors());
 app.use(express.json());
